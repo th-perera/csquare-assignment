4d55d2fdf463226c72cee3ace0c4a64ac88bc67e
unknown <it21165498@my.sliit.lk>
2024-02-23 01:10:24 +0530

    remove downloaded boostrap css

---
+++ items.php
@@ -0,0 +1,13 @@
+<?php 
+include('config/dbcon.php'); 
+include('includes/header.php'); 
+?>
+
+<!-- main content start -->
+
+
+<!-- end of main -->
+<?php 
+include('includes/footer.php'); 
+include('includes/scripts.php'); 
+?>
