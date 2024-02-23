3da83b316eeb2e25c67b7396de9387e324b23c13
unknown <it21165498@my.sliit.lk>
2024-02-23 16:59:15 +0530

    added items retrieve

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -32,8 +32,8 @@
                     <div class="collapse" id="itemCollapseLayouts" aria-labelledby="headingOne"
                         data-bs-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="layout-static.html">All Items</a>
-                            <a class="nav-link" href="layout-sidenav-light.html">Add Item</a>
+                            <a class="nav-link" href="items.php">All Items</a>
+                            <a class="nav-link" href="items.php">Add Item</a>
                         </nav>
                     </div>
                     <!--  -->
--- items.php
+++ items.php
@@ -4,10 +4,135 @@ include('includes/header.php');
 ?>
 
 <!-- main content start -->
+<!-- main content start -->
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Items</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">Items > All Items</li>
+    </ol>
+    <?php 
+    // if (isset($_GET['msg'])) {
+    //     $msg = $_GET['msg'];
 
+    //     echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    //         '.$msg.'
+    //         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    //         </div>';
+    // }
+    
+    ?>
+    <!-- ? -->
+    <div class="card mb-4">
+        <div class="card-header">
+            <i class="fas fa-user me-1"></i>
+            All Items
+        </div>
+        <div class="card-body" id="table">
+            <table id="datatablesSimple">
+                <thead>
+                    <tr>
+                        <th>Item Code</th>
+                        <th>Item Category</th>
+                        <th>Item Subcategory</th>
+                        <th>Item Name</th>
+                        <th>Quantity</th>
+                        <th>Unit Price </th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT i.id AS item_id, i.*, ic.*, isc.*
+                                FROM item i
+                                JOIN item_category ic ON i.item_category = ic.id
+                                JOIN item_subcategory isc ON i.item_subcategory = isc.id;
+                                ";
+                    $result = mysqli_query($conn, $query);
+                    if(mysqli_num_rows($result) > 0) {
+                        
+                        foreach($result as $item) {
+
+                        ?>
+                    <tr>
+                        <td>
+                            <?= $item['item_code'] ?>
+                        </td>
+                        <td><?= $item['category'] ?></td>
+                        <td><?= $item['sub_category'] ?></td>
+                        <td><?= $item['item_name'] ?></td>
+                        <td><?= $item['quantity'] ?></td>
+                        <td><?= $item['unit_price'] ?></td>
+                        <td>
+                            <a href="edit-item.php?id=<?= $item['item_id'] ?>">
+                                <i class="fa-solid fa-pen-to-square fs-4 me-3"></i>
+                            </a>
+                            <button type="button" class="btn btn-link link-danger delete-btn"
+                                value="<?= $item['item_id'] ?>">
+                                <i class="fa-solid fa-trash fs-4"></i>
+                            </button>
+                        </td>
+
+                    </tr>
+                    <?php
+                    }
+
+                    } else {
+                    echo "<h3>No Records Found !</h3>";
+                    }
+
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 <!-- end of main -->
 <?php 
 include('includes/footer.php'); 
 include('includes/scripts.php'); 
 ?>
+
+
+
+
+<!-- popup alert -->
+<script>
+$(document).ready(function() {
+    $('.delete-btn').on('click', function(e) {
+        e.preventDefault();
+
+        var id = $(this).val();
+
+        swal({
+                title: "Are you sure?",
+                text: "Once deleted, you will not be able to recover",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+                    $.ajax({
+                        method: 'POST',
+                        url: 'delete-customer.php',
+                        data: {
+                            'id': id,
+                            'delete-btn': true
+                        },
+                        success: function(response) {
+                            swal("Success!", "Product deleted successfully!",
+                                    "success")
+                                .then(function() {
+                                    // Reload the page without any URL parameters
+                                    window.location.href = window.location.pathname;
+                                });
+                        },
+                    })
+                } else {
+
+                }
+            });
+    })
+});
+</script>
